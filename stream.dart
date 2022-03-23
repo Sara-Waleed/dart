d28778f1e51dfdf3c6150c3b85722515f37b7bae
Sara-Waleed <92535228+Sara-Waleed@users.noreply.github.com>
2022-03-23 20:32:54 +0200

    Add files via upload
    last version

--- stream.dart
+++ stream.dart
@@ -1,1 +1,15 @@
+void main() {
+  final s = Stream.periodic(Duration(milliseconds: 500), (a) => a);
 
+  final sub = s.listen((_) => _);
+  sub.onData((data) {
+    dat > 10 ? sub.cancel() : print(data);
+  });
+
+  sub.onData(() {
+    print('Done!');
+  });
+
+  final s2 = s.where((event) => event % 2 == 0);
+  s2.listen((event) => print(event));
+}
