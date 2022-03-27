2ded9f1d531ac7aca744a3c76af43b9e84849d69
Sara-Waleed <92535228+Sara-Waleed@users.noreply.github.com>
2022-03-27 21:41:11 +0200

    Add files via upload

--- nullsafety.dart
+++ nullsafety.dart
@@ -1,1 +1,16 @@
+void main() {
+  var sara = person();
+  print(sara.age);
+  print(sara.favoritecoloe);
+  sara.setbackground('yellow');
+}
 
+class person {
+  var age = 21;
+  // ignore: avoid_init_to_null
+  var favoritecoloe = null;
+
+  void setbackground(var color) {
+    print(color);
+  }
+}
